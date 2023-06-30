f08ca65af58560abe1e53dcb61488179cd751a89
Artem <artem.matseruk@gmail.com>
2023-06-30 18:40:34 +0300

    first commit

---
+++ pages/api/orders/[id]/index.js
@@ -0,0 +1,16 @@
+import { getSession } from 'next-auth/react';
+import db from "../../../utils/db";
+import Order from "../../../models/OrderModel";
+
+const handler = async (req, res) => {
+	const session = await getSession({ req });
+	if (!session) {
+		return res.status(401).json({ error: 'Not authenticated' });
+	}
+	await db.dbConnect();
+	const order = await Order.findById(req.query.id);
+	await db.dbDisconnect();
+	res.send(order);
+};
+
+export default handler;
